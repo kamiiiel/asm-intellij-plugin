9e6b07f9b51d05a9925742b7cdd71c174f3c152c
Kamiel Ahmadpour <kamiel.ahmadpour@graviteesource.com>
2023-07-26 19:03:25 +0100

    fix: update to asm 9.5

--- src/main/java/org/objectweb/asm/idea/plugin/view/BytecodeOutlineToolWindowFactory.java
+++ src/main/java/org/objectweb/asm/idea/plugin/view/BytecodeOutlineToolWindowFactory.java
@@ -22,16 +22,17 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * ASM ToolWindow factory
  */
 public class BytecodeOutlineToolWindowFactory implements ToolWindowFactory {
 
-    public void createToolWindowContent(final Project project, final ToolWindow toolWindow) {
-        toolWindow.getContentManager().addContent(ContentFactory.SERVICE.getInstance().createContent(BytecodeOutline.getInstance(project), "Bytecode", false));
-        toolWindow.getContentManager().addContent(ContentFactory.SERVICE.getInstance().createContent(BytecodeASMified.getInstance(project), "ASMified", false));
-        toolWindow.getContentManager().addContent(ContentFactory.SERVICE.getInstance().createContent(GroovifiedView.getInstance(project), "Groovified", false));
+    public void createToolWindowContent(final @NotNull Project project, final ToolWindow toolWindow) {
+        toolWindow.getContentManager().addContent(ContentFactory.getInstance().createContent(BytecodeOutline.getInstance(project), "Bytecode", false));
+        toolWindow.getContentManager().addContent(ContentFactory.getInstance().createContent(BytecodeASMified.getInstance(project), "ASMified", false));
+        toolWindow.getContentManager().addContent(ContentFactory.getInstance().createContent(GroovifiedView.getInstance(project), "Groovified", false));
     }
 
 }
